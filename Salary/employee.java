47925cc8ae35e40697420bbcf23dc5c1b94187fe
Mitesh <miteshtagadiya@gmail.com>
2018-07-27 20:33:51 +0530

    employee Salary

---
+++ Salary/employee.java
@@ -0,0 +1,31 @@
+package Salary;
+
+public class employee {
+    private String firstname;
+    private String lastname;
+    private int ssn;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+}
