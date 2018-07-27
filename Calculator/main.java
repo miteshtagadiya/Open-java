25e290f01bb582ad8144d39a4add75abacd94d3e
Mitesh <miteshtagadiya@gmail.com>
2018-07-27 20:33:25 +0530

    Complex main

---
+++ Calculator/main.java
@@ -0,0 +1,9 @@
+package Calculator;
+
+public class main {
+    public static void main(String[] args) {
+        Calculator c = new Calculator(120.12);
+        c.add(12.12);
+        c.display();
+    }
+}
